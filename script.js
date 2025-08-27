a99bcf1399919fdfc3b3e46d3e678cc06facd2a0
dawood125 <dawood.bhatti8812@gmail.com>
2025-08-27 04:42:36 -0700

    Drum kit completed

---
+++ script.js
@@ -0,0 +1,12 @@
+function PlaySound(e) {
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+  setTimeout(() => {
+    key.classList.remove("playing");
+  }, 200);
+}
+window.addEventListener("keydown", PlaySound);
